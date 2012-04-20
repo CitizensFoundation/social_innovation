f449bca4d6467f2496ee3dac4e835a97cbb8a36b
Robert Bjarnason <rbjarnason>
2012-04-20 17:28:38 +0000

    Initial Commit

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ end
 
 module SocialInnovation
   class Application < Rails::Application
+
+    require 'core_extensions'
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
