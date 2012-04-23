4a2e846773bfd07493ce57730cba84917f703072
Robert Bjarnason <rbjarnason>
2012-04-23 00:22:17 +0000

    Reaploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -57,7 +57,7 @@ after "deploy", "delayed_job:restart"
 
 namespace :assets do
   task :precompile, :roles => :web, :except => { :no_release => true } do
-    if true or capture("cd #{latest_release} && #{source.local.log(source.next_revision(current_revision))} vendor/assets/ app/assets/ lib/assets/ | wc -l").to_i > 0
+    if capture("cd #{latest_release} && #{source.local.log(source.next_revision(current_revision))} vendor/assets/ app/assets/ lib/assets/ | wc -l").to_i > 0
       run "cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile"
     else
       logger.info "No changes on assets. Skipping pre-compilation."
