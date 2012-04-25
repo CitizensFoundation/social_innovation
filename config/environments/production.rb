6700dabb0b7e9638b3aebe3c7c29d94581bea7af
Robert Bjarnason <robert.bjarnason@gmail.com>
2012-04-25 05:53:33 +0100

    Assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,10 +15,10 @@ SocialInnovation::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Generate digests for assets URLs
-  config.assets.digest = true
+  config.assets.digest = false
 
   # Specifies the header that your server uses for sending files
   config.action_dispatch.x_sendfile_header = "X-Sendfile"
