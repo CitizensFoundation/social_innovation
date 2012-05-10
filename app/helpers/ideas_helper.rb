aaaecb6d5f6bcaa659b9a3ebb14faed4f1771c12
Robert Bjarnason <rbjarnason>
2012-05-10 13:40:28 +0000

    Fixes for css

--- app/helpers/ideas_helper.rb
+++ app/helpers/ideas_helper.rb
@@ -2,9 +2,9 @@ module IdeasHelper
 
   def get_points_number_text(points_count, total_points,new_points=false)
     if points_count>0
-      "<span style=\"color:#666;font-size:0.75em;\"> (#{points_count} af #{total_points})</span>".html_safe
+      "(#{points_count} #{tr("of","points")} #{total_points})".html_safe
     else
-      "<span style=\"color:#666;font-size:0.75em;\"> (#{new_points ? tr("no new points", "helper/ideas") : tr("no points", "helper/ideas")})</span>".html_safe
+      "(#{new_points ? tr("no new points", "helper/ideas") : tr("no points", "helper/ideas")})".html_safe
     end
   end
 end
